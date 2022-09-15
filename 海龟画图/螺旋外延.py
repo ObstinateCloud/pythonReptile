349941edd9db0b1bf0ddac159b6f387a039f446d
zjyselfpc <zjyselfpc@person>
2022-09-15 22:31:54 +0800

    海龟画图

--- 海龟画图/螺旋外延.py
+++ 海龟画图/螺旋外延.py
@@ -3,35 +3,36 @@ from turtle import *
 import time
 import os
 
-# def huaZhengFangXing(gui,bianChang):
-#     gui.pendown()
-#     gui.begin_fill()
-#     for i in range(4):
-#         gui.fd(bianChang)
-#         gui.right(90)
-#     gui.end_fill()
-#     gui.penup()
-#
-# yanSeBiao = ["red","yellow","orange","blue","green"]
-# t = Turtle()
-# t.speed(0)
-# screen = t.getscreen()
-# screen.bgcolor("black")
-#
-# xuHao = 0
-# jiaodu = 5
-# for i in range(150):
-#     jiaodu -=1
-#     t.pencolor(yanSeBiao[xuHao])
-#     t.fillcolor(yanSeBiao[xuHao])
-#     huaZhengFangXing(t,10)
-#     t.fd(20)
-#     t.right(jiaodu)
-#     #数组越界
-#     xuHao = xuHao + 1
-#     if xuHao == 5:
-#         xuHao = 0
+def huaZhengFangXing(gui,bianChang):
+    gui.pendown()
+    gui.begin_fill()
+    for i in range(4):
+        gui.fd(bianChang)
+        gui.right(90)
+    gui.end_fill()
+    gui.penup()
+
+yanSeBiao = ["red","yellow","orange","blue","green"]
 t = Turtle()
+t.speed(0)
+screen = t.getscreen()
+screen.bgcolor("black")
+
+xuHao = 0
+jiaodu = 45
+step = 30
+for i in range(45):
+    t.pencolor(yanSeBiao[xuHao])
+    t.fillcolor(yanSeBiao[xuHao])
+    huaZhengFangXing(t,10)
+    t.fd(step)
+    t.right(45)
+    step = step+5
+    # step = step+i
+    #数组越界
+    xuHao = xuHao + 1
+    if xuHao == 5:
+        xuHao = 0
 
 # 图案1
 # for i in range(1, 110, 2):
@@ -56,8 +57,8 @@ t = Turtle()
 #     t.down()
 
 # 同心圆
-for i in range(10,110,10):
-    t.circle(i)
-    t.up()
-    t.goto(0,-i)
-    t.down()
+# for i in range(10,110,10):
+#     t.circle(i)
+#     t.up()
+#     t.goto(0,-i)
+#     t.down()
