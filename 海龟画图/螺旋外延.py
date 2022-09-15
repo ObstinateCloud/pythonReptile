b3e43659b7316a0f9a5b0dde18492b6896c11e03
zhangjianyun <zhangjianyun_kd@163.com>
2022-09-15 17:23:30 +0800

    增加海龟画图示例

--- 海龟画图/螺旋外延.py
+++ 海龟画图/螺旋外延.py
@@ -1,6 +1,8 @@
-#螺旋彩图
+# 螺旋彩图
 from turtle import *
 import time
+import os
+
 # def huaZhengFangXing(gui,bianChang):
 #     gui.pendown()
 #     gui.begin_fill()
@@ -29,4 +31,33 @@ import time
 #     xuHao = xuHao + 1
 #     if xuHao == 5:
 #         xuHao = 0
+t = Turtle()
 
+# 图案1
+# for i in range(1, 110, 2):
+#     t.circle(i, i)
+#     t.goto(0, 0)
+# 图案2 蚊香形状
+# for i in range(1, 110, 2):
+#     t.circle(i, i)
+
+# 图案3
+# for i in range(1, 110, 2):
+#     t.circle(i, i)
+#     t.up()
+#     t.goto(0, 0)
+#     t.down()
+
+# 图案4
+# for i in range(1, 110, 2):
+#     t.circle(i, 110-i)
+#     t.up()
+#     t.goto(0, 0)
+#     t.down()
+
+# 同心圆
+for i in range(10,110,10):
+    t.circle(i)
+    t.up()
+    t.goto(0,-i)
+    t.down()
