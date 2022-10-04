f725713be3b851dbd1513b8fe71abd183bc9d632
zjyselfpc <zjyselfpc@person>
2022-10-04 20:55:49 +0800

    1.crawlspider的使用

---
+++ scrapy_crawlspider_06/scrapy_crawlspider_06/spiders/readbook.py
@@ -0,0 +1,31 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from scrapy_crawlspider_06.items import ScrapyCrawlspider06Item
+
+
+class ReadbookSpider(CrawlSpider):
+    name = 'readbook'
+    allowed_domains = ['www.dushu.com']
+    start_urls = ['https://www.dushu.com/book/1175_1.html']
+
+    # rules = (
+    #     Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    # )
+
+    rules = (
+        Rule(LinkExtractor(allow=r'/book/1175_\d+\.html'), callback='parse_item', follow=False),
+    )
+
+    def parse_item(self, response):
+        print('读书网')
+        img_list = response.xpath('//div[@class="bookslist"]/ul/li//div[@class="book-info"]//img')
+        for img in img_list:
+            # print(img)
+            img_src = img.xpath('./@data-original').extract_first()
+            # if img_src:
+            #     img_src = img.xpath('./@src').extract_first()
+            book_name = img.xpath('./@alt').extract_first()
+            book = ScrapyCrawlspider06Item(img_src=img_src, book_name=book_name)
+            yield book
